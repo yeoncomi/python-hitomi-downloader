00bcadb3aaf59d49c94d2ed78379a32d4fffc870
Yeoncomi <yeoncomi@gmail.com>
2018-01-12 22:22:08 +0900

    commit
    multi-threading

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import json
 import requests
 import urllib
-import shutil
-hitomi_number = input("히토미 번호를 입력해주세요: ")
-Hitomi_request = requests.get('https://hitomi.la/galleries/'+hitomi_number+'.js')
-Hitomi_js = Hitomi_request.text.replace("var galleryinfo = ","")
-Hitomi_list = json.loads(Hitomi_js)
-for x in Hitomi_list:
-    print(x["name"])
-    urllib.request.urlretrieve("https://0a.hitomi.la/galleries/"+str(hitomi_number)+"/"+str(x["name"]), "download/"+str(x["name"]))
-shutil.make_archive(str(hitomi_number), 'zip', 'download')
+import threading
+import sys
+import os
+
+def croll(data, hitomi_no):
+    print("https://0a.hitomi.la/galleries/" + hitomi_no + "/" + data)
+    urllib.request.urlretrieve("https://0a.hitomi.la/galleries/" + hitomi_no + "/" + data, "download/" + data)
+
+if(__name__ == '__main__'):
+    hitomi_no = str(input("Enter the hitomi(exthntai) number: "))
+    Hitomi_request = requests.get('https://hitomi.la/galleries/' + hitomi_no + '.js')
+    Hitomi_js = Hitomi_request.text.replace("var galleryinfo = ","")
+    Hitomi_list = json.loads(Hitomi_js)
+
+    if(not os.path.exists('download')):
+        os.makedirs('download')
+
+    for data in Hitomi_list:
+        print(data["name"] + ' / ' + hitomi_no)
+        threading.Thread(target = croll, args = [data["name"], hitomi_no]).start()
+        print("process " + data["name"] + " started")
+print("Download Done")
+sys.exit(1)
